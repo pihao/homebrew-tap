2b0af7e6f4a73c81dd463fb4b779949478033a96
pihao <kissyid@gmail.com>
2018-05-18 11:44:03 +0800

    Add himawari8-desktop 0.0.2

--- himawari8-desktop.rb
+++ himawari8-desktop.rb
@@ -1,8 +1,8 @@
 class Himawari8Desktop < Formula
   desc "Cool wallpaper."
-  homepage "https://himawari8.nict.go.jp/"
+  homepage "https://github.com/pihao/himawari8-desktop"
   url "https://github.com/pihao/himawari8-desktop/releases/download/0.0.2/himawari8-desktop-0.0.2.tar.gz"
-  sha256 "060ae3f9fccd1cf3667506ce8e04e0719a314a04e6e19822b93c35409c089bab"
+  sha256 "df13cd9dc523580d9f40a6c0c143cf12dea798e0f55b7700f129c5cf121962c2"
   version "0.0.2"
 
   def install

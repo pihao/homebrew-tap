bfd5f1a4c5836837166e75675e3cd5a01a03b106
pihao <kissyid@gmail.com>
2022-09-25 05:08:38 +0800

    add himawari8 bottle

--- Formula/himawari8.rb
+++ Formula/himawari8.rb
@@ -5,9 +5,17 @@ class Himawari8 < Formula
   desc "Himawari8 wallpaper for MacOS"
   homepage "https://himawari.asia"
   url "https://github.com/pihao/himawari8/archive/refs/tags/0.0.3.tar.gz"
-  sha256 "72402042a20b9f1a9f52f83a65d311a099790a3c070a289c575f3c4c1bcf86bf"
-  version "0.0.3-9"
+  sha256 "466fd130a0b5486fc0def35b76523e951c0adb0f1cb1c020a831fcfa0d6c61e8"
+  version "0.0.3-10"
   license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://github.com/pihao/homebrew-tap/releases/download/himawari8-0.0.3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b8194d1ab1d0a8f35488b33a3ff9934aab69f899cc9ebf64edf9d50a5726a2fb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b8194d1ab1d0a8f35488b33a3ff9934aab69f899cc9ebf64edf9d50a5726a2fb"
+    sha256 cellar: :any_skip_relocation, monterey:       "426d2957327ff3f232344626774a5bd4aea420064089c45e4e376991bb431c37"
+    sha256 cellar: :any_skip_relocation, big_sur:        "426d2957327ff3f232344626774a5bd4aea420064089c45e4e376991bb431c37"
+  end
 
   depends_on "go" => :build
 
